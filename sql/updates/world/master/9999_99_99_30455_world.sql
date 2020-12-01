0180334cd44d918a6b818a29c203e1b3fed566ad
Ryzen <972481+Ryzen-0@users.noreply.github.com>
2020-12-01 20:55:46 +0100

    sql tips

--- sql/updates/world/master/9999_99_99_30455_world.sql
+++ sql/updates/world/master/9999_99_99_30455_world.sql
@@ -1,3 +1,3 @@
 -- mage ice lance
-DELETE FROM spell_script_names WHERE spell_id IN (30455);
-INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (30455, 'spell_mage_ice_lance');
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_mage_ice_lance';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (30455, 'spell_mage_ice_lance');
